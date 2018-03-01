28591e0937d012fba92fcff7998b97d8e960b7f4
sveiaam <35837663+sveiaam@users.noreply.github.com>
2018-03-01 16:00:57 +0100

    Finished quest 1

--- q1.py
+++ q1.py
@@ -1,18 +1,35 @@
 import Protein as prot
 
-P = prot.Protein(10)
+'''
+We chose some specific rotations to better show off that the protein folding code worked
+To chose randomly, we would write something like:
 
-P.draw()
+import random
+num = random.randint(1, SIZE)
+clockwise = random.randint(0, 1)
+
+Inserting num and clockwise as parameters for the protein twist, then generating some new num and
+clockwise, and inserting them again. 
+'''
+
+SIZE = 10
+
+# Initialize
+P = prot.Protein(SIZE)
+
+# Show the initial state. present() plots the protein with matplotlib, and draw() prints the matrix to output
 P.present()
+P.draw()
 print()
 
+# Show state after 1 twist
 P.twist(8, False)
-P.draw()
 P.present()
+P.draw()
 print()
 
+# Show state after 2 twists
 P.twist(6, True)
-P.draw()
 P.present()
+P.draw()
 print()
-
