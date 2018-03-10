f4e0285f475dc4b1c2b48d69f4b5e21e3f387071
Marius Sunde Sivertsen <marius.sunde1997@gmail.com>
2018-03-10 16:44:40 +0100

    Updated q4.py

--- q4.py
+++ q4.py
@@ -21,7 +21,7 @@ def quest_4_1(acc,protLength):
 	# x-axis of plot
 	twistValues = np.linspace(0, 30000, acc)
 	# y-axis of plot
-	Evalues = np.zeros(acc)
+	Evalues = np.zeros(len(twistValues))
 
 	dist = int(twistValues[1] - twistValues[0])
 
@@ -31,7 +31,7 @@ def quest_4_1(acc,protLength):
 	# Plot while temperature is positive
 	while T > 0:
 		# For every plotting point
-		for i in range(acc):
+		for i in range(len(twistValues)):
 			# Inbetween plotting points
 			for j in range(dist):
 				# Check if temperature needs to be updated
@@ -42,7 +42,7 @@ def quest_4_1(acc,protLength):
 				counter += 1
 			Evalues[i] = P.E
 
-			print("Iter: ",i + 1,"/",acc)
+			print("Iter: ",i + 1,"/",len(twistValues))
 
 	# Plot
 	plt.plot(twistValues, Evalues, lw=0.3, label=r"15 monomers", color="crimson")
@@ -63,9 +63,9 @@ def quest_4_2(acc,protLength):
 
 	#x and y-values to plot (temperature and avg. energy)
 	temp = np.linspace(1500,1e-12,acc)
-	Evalues = np.zeros(acc)
+	Evalues = np.zeros(len(twistValues))
 
-	for i in range(acc):
+	for i in range(len(twistValues)):
 		#perform 600 twists at every temperature, record avg-energy at every temp
 		T = temp[i]
 		energy = np.zeros(numTwists)
@@ -77,7 +77,7 @@ def quest_4_2(acc,protLength):
 		#Add average energy in this temperature to plotting-vector
 		Evalues[i] = np.average(energy)
 		#Print current state:
-		print("Iter: ",i+1,"/",acc, "\tTemp: ",T)
+		print("Iter: ",i+1,"/",len(twistValues), "\tTemp: ",T)
 
 	plt.plot(temp, Evalues, lw=0.3, label=r"15 monomers", color="crimson")
 	plt.xlabel(r"$T$  [K]", size=20)
@@ -92,9 +92,9 @@ def quest_4_3(acc,protLength):
 
 	# x and y-values to plot (temperature and avg. proteinlength)
 	temp = np.linspace(1500, 1e-12, acc)
-	Lvalues = np.zeros(acc)
+	Lvalues = np.zeros(len(twistValues))
 
-	for i in range(acc):
+	for i in range(len(twistValues)):
 		# perform 600 twists at every temperature, record avg-energy at every temp
 		T = temp[i]
 		lengths = np.zeros(numTwists)
@@ -106,7 +106,7 @@ def quest_4_3(acc,protLength):
 		# Add average max-diameter in this temperature to plotting-vector
 		Lvalues[i] = np.average(lengths)
 		# Print current state:
-		print("Iter: ",i + 1, "/", acc, "\tTemp: ",T)
+		print("Iter: ",i + 1, "/", len(twistValues), "\tTemp: ",T)
 
 	plt.plot(temp, Lvalues, lw=0.3, label=r"15 monomers", color="crimson")
 	plt.xlabel(r"$T$  [K]", size=20)
@@ -125,14 +125,14 @@ def quest_4_4(acc,protLength,numbOfCoolDowns):
 	temp = np.linspace(1500, 1e-12, acc)
 
 	for k in range(numbOfCoolDowns):
-		for i in range(acc):
+		for i in range(len(twistValues)):
 			# perform 600 twists at every temperature, record avg-energy at every temp
 			T = temp[i]
 			for j in range(numTwists):
 				# Twisting
 				protein = prot.randomTwist(protein, T)
 			# Print current state:
-			print("Iter: ", i + 1, "/", acc, "\tTemp: ", T)
+			print("Iter: ", i + 1, "/", len(twistValues), "\tTemp: ", T)
 	protein.draw()
 
 #quest_4_1(acc1,protLength)
